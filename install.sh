12640bbbb20a586566efe7b6094a5aa36ef0d036
RomanNum3ral <116513200+RomanNum3ral@users.noreply.github.com>
2024-10-25 09:39:07 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -55,7 +55,3 @@ cd
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt install code
-sudo chmod +x setup.sh
-sudo ./setup.sh
-
-
