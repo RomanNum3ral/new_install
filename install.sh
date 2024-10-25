fafd5d7725afc05cdd46d7118e57ea15f4043a32
RomanNum3ral <116513200+RomanNum3ral@users.noreply.github.com>
2024-10-25 08:51:23 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ sudo apt dist-upgrade -y
 sudo apt full-upgrade -y
 sudo apt autoremove -y
 sudo apt install apt-transport-https curl -y
-sudo apt install libfuse2 libykpers-1-1 libyubikey-udev libwxbase3.0-0v5 software-properties-common apt-transport-https wget libwxgtk3.0-gtk3-0v5 htop npm backdoor-factory bleachbit stegosuite terminator gedit gobuster sherlock ncdu hollywood cmatrix flameshot steghide exiftool remmina virtualbox torbrowser-launcher keepassxc bettercap 2to3 ffmpeg handbrake freecad tilix openscad vlc libreoffice backdoor-factory mingw-w64 -y
+sudo apt install libfuse2 libykpers-1-1 libyubikey-udev software-properties-common apt-transport-https wget htop npm 
+sudo apt install -y backdoor-factory bleachbit stegosuite terminator gedit gobuster sherlock ncdu hollywood cmatrix flameshot steghide exiftool remmina 
+sudo apt install -y virtualbox torbrowser-launcher keepassxc bettercap 2to3 ffmpeg handbrake freecad tilix openscad vlc libreoffice backdoor-factory mingw-w64 -y
 sudo apt-get install -y linux-headers-$(uname -r)
 cd
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
@@ -22,9 +24,6 @@ cd
 sudo wget https://download2.tixati.com/download/tixati_3.16-1_amd64.deb
 sudo chmod +x tixati_3.16-1_amd64.deb
 sudo dpkg -i tixati_3.16-1_amd64.deb
-cd
-wget https://discord.com/api/download?platform=linux&format=deb
-sudo dpkg -i discord-0.0.67.deb
 cd
 sudo wget https://download.sublimetext.com/sublime-text_build-3211_amd64.deb
 sudo chmod +x sublime-text_build-3211_amd64.deb
