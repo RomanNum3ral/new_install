d326e28fa8286df1896203f91cba1f1ff321e358
RomanNum3ral <116513200+RomanNum3ral@users.noreply.github.com>
2024-10-26 11:42:52 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,8 +13,6 @@ sudo apt-get install -y linux-headers-$(uname -r)
 cd
 sudo add-apt-repository ppa:obsproject/obs-studio
 sudo apt install -y obs-studio
-sudo apt install -y ocl-icd-opencl-dev fakeroot xorriso
-
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
